18aa8119ac412e6c15bbd75453720ae62dd85a98
Victor Yakimov <victor.yakimov@gmail.com>
2024-05-02 15:55:31 +0200

    zsh

--- install_packages.sh
+++ install_packages.sh
@@ -3,4 +3,6 @@ sudo apt install -y software-properties-common &&\
 sudo add-apt-repository -y ppa:apptainer/ppa &&\
 sudo apt update &&\
 sudo apt install -y apptainer &&\
-sudo apt install neovim
+sudo apt install -y neovim &&\
+sudo apt install -y zsh &&\
+sudo apt upgrade -y
